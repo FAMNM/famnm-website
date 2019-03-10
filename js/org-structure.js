42400f0b8f953ca42a80ad0b02d87a57999ddabf
Paul Bonnen <hal7df@gmail.com>
2019-03-10 17:18:16 -0400

    org structure: start working on an interactive org chart

---
+++ js/org-structure.js
@@ -0,0 +1,276 @@
+orgData = [
+    {
+        id: "#svg-president",
+        title: "President",
+        placement: "bottom",
+        content: "The President is responsible for setting the "
+            + "overall strategy and direction of the organization, as well "
+            + "as organizing general body and FAMNM planning meetings, and "
+            + "maintains communication with FIRST and other FIRST "
+            + "collegiate groups."
+    },
+    {
+        id: "#svg-vp",
+        title: "Vice President",
+        placement: "bottom",
+        content: "The Vice President is responsible for all "
+            + "communication with the University of Michigan and student "
+            + "organizations with which FAMNM is affiliated, ensures that "
+            + "FAMNM is in compliance with University policies, and works "
+            + "with the Director of Internal Affairs to reach out to all "
+            + "FIRST alumni at the University of Michigan."
+    },
+    {
+        id: "#svg-dir-int",
+        title: "Director of Internal Affairs",
+        placement: "bottom",
+        content: "The Director of Internal Affairs is responsible for "
+            + "managing membership in FAMNM, maintaining the FAMNM Slack "
+            + "workspace and email lists, and managing the onboarding "
+            + "process for new members."
+    },
+    {
+        id: "#svg-dir-marketing",
+        title: "Director of Marketing",
+        placement: "right",
+        content: "The Director of Marketing is responsible for "
+            + "overseeing the activitiese of the Marketing Committee, "
+            + "specifically: updating and managing FAMNM's social media "
+            + "accounts, maintaining branding standards, and ordering "
+            + "FAMNM swag."
+    },
+    {
+        id: "#svg-treasurer",
+        title: "Treasurer",
+        placement: "right",
+        content: "The Treasurer is responsible for overseeing the "
+            + "Treasury Committee, and ensuring that its responsibilities "
+            + "are duly executed. Also responsible for approving "
+            + "reimbursements, ensuring the continuity of the UM FIRST "
+            + "Scholarship, and overseeing all of FAMNM's income and "
+            + "expenditures."
+    },
+    {
+        id: "#svg-dir-co",
+        title: "Director of Corporate Outreach",
+        placement: "right",
+        content: "The Director of Corporate Outreach is responsible "
+            + "for overseeing the activites of the Corporate Outreach "
+            + "Committee and maintains communication with FAMNM's partner"
+            + "corporations."
+    },
+    {
+        id: "#svg-dir-pd",
+        title: "Director of Project Development",
+        placement: "right",
+        content: "The Director of Project Development is responsible "
+            + "for organizing and overseeing the activities of the "
+            + "Project Development Committee, as well as ensuring that "
+            + "all FAMNM events run smoothly."
+    },
+    {
+        id: "#svg-kickoff-coord",
+        title: "Kickoff Coordinator",
+        placement: "left",
+        content: "The Kickoff Coordinator is responsible for "
+            + "organizing and managing all kickoff-related events run "
+            + "by FAMNM, such as FRC Kickoff, FTC Kickoff, or FRC Robot "
+            + "Quick Build. Works with the Project Development Committee "
+            + "to continually improve all kickoff events."
+    },
+    {
+        id: "#svg-ri3d-coord",
+        title: "Ri3D Coordinator",
+        placement: "left",
+        content: "The Ri3D coordinator is responsible for organizing "
+            + "the FAMNM Ri3D team as well as overseeing fundraising and "
+            + "recruitment for the team. The Ri3D coordinator is not "
+            + "responsible for running the team during the Ri3D build and "
+            + "competition days, but makes sure the team is well-equipped "
+            + "during both build and competition."
+    },
+    {
+        id: "#svg-frc-coord",
+        title: "FRC Coordinator",
+        placement: "left",
+        content: "The FRC Coordinator is FAMNM's point of contact for "
+            + "all affiliated FRC teams, and works to provide affiliated "
+            + "FRC teams with any resources available to FAMNM. Ensures "
+            + "that all of FAMNM's FRC mentors have completed appropriate "
+            + "trainings and paperwork."
+    },
+    {
+        id: "#svg-ftc-coord",
+        title: "FTC Coordinator",
+        placement: "left",
+        content: "The FTC Coordinator is FAMNM's point of contact for "
+            + "all affiliated FTC teams, and works to provide affiliated "
+            + "FTC teams with any resources available to FAMNM. Ensures "
+            + "that all of FAMNM's FTC mentors have completed appropriate "
+            + "trainings and paperwork."
+    },
+    {
+        id: "#svg-fll-coord",
+        title: "FLL Coordinator",
+        placement: "left",
+        content: "The FLL Coordinator is FAMNM's point of contact for "
+            + "all affiliated FLL teams, and works to provide affiliated "
+            + "FLL teams with any resources available to FAMNM. Ensures "
+            + "that all of FAMNM's FLL mentors have completed appropriate "
+            + "trainings and paperwork."
+    },
+    {
+        id: "#svg-volunteer-coord",
+        title: "Volunteer Coordinator",
+        placement: "left",
+        content: "The Volunteer Coordinator is responsible for "
+            + "managing all FAMNM volunteers and organizing logistics for"
+            + "FAMNM's volunteer operations."
+    },
+    {
+        id: "#svg-exec-board",
+        title: "Executive Board",
+        placement: "left",
+        content: "The Executive Board consists of all critical members "
+            + "of the FAMNM Core Team. As a whole, they are responsible "
+            + "for managing FAMNM's organizational resources."
+    },
+    {
+        id: "#svg-core-team",
+        title: "Core Team",
+        placement: "left",
+        content: "The Core Team consists of all of FAMNM's elected "
+            + "positions."
+    },
+    {
+        id: "#svg-committee",
+        title: "Committees",
+        placement: "left",
+        content: "Committees are subgroups within FAMNM that are "
+            + "focused on specific components of FAMNM's operations."
+    },
+    {
+        id: "#svg-managed-group",
+        title: "Managed Groups",
+        placement: "left",
+        content: "Managed Groups are interest groups within FAMNM that "
+            + "are typically focused on a specific type of outreach "
+            + "towards the FIRST community, such as mentoring or " 
+            + "volunteering. While they are not responsible for making any "
+            + "organizational decisions, they are usually managed by one "
+            + "or more members of the Core Team."
+    },
+    {
+        id: "#svg-webmaster",
+        title: "Webmaster",
+        placement: "right",
+        content: "The Webmaster is responsible for overseeing all "
+            + "websites managed by FAMNM, as well as managing the FAMNM "
+            + "Website Committee."
+    },
+    {
+        id: "#svg-website-comm",
+        title: "Website Committee",
+        placement: "right",
+        content: "The Website Committee is responsible for maintaining "
+            + "the FAMNM website and other FAMNM-managed website projects."
+    },
+    {
+        id: "#svg-marketing-comm",
+        title: "Marketing Committee",
+        placement: "right",
+        content: "The Marketing Committee is responsible for "
+            + "maintaining FAMNM's social media accounts, producing "
+            + "marketing materials for events at which FAMNM participates, "
+            + "maintaining and updating FAMNM branding standards, and "
+            + "desigining FAMNM swag."
+    },
+    {
+        id: "#svg-treasury-comm",
+        title: "Treasury Committee",
+        placement: "right",
+        content: "The Treasury Committee is responsible for balancing "
+            + "and maintaining FAMNM's budget, applying for funding for "
+            + "FAMNM events, managing reimbursements of FAMNM members, "
+            + "and working on the University of Michigan FIRST Scholarship."
+    },
+    {
+        id: "#svg-co-comm",
+        title: "Corporate Outreach Committee",
+        placement: "right",
+        content: "The Corporate Outreach Committee is responsible for "
+            + "managing FAMNM's corporate sponsorships, maintaining "
+            + "communication with FAMNM alumni, and providing professional "
+            + "development opportunities for FAMNM members."
+    },
+    {
+        id: "#svg-alums",
+        title: "FIRST Alumni Network at UofM",
+        placement: "top",
+        content: "This group consists of all FIRST Alumni that are "
+            + "students or alumni of the University of Michigan, and are "
+            + "known to FAMNM. Members of this group need not be general "
+            + "members, and receive email updates about FAMNM's operations "
+            + "and professional development opportunities."
+    },
+    {
+        id: "#svg-pd-comm",
+        title: "Project Development Committee",
+        placement: "right",
+        content: "The Project Development committee is responsible for "
+            + "creating new FAMNM events and improving existing FAMNM "
+            + "events."
+    },
+    {
+        id: "#svg-ri3d-comm",
+        title: "Ri3D Team",
+        placement: "left",
+        content: "The Ri3D team consists of all individuals that "
+            + "intend to participate in or help with the FAMNM Robot in 3 Days "
+            + "endeavor. The Ri3D Team is responsible for its own "
+            + "fundraising and recruitment."
+    },
+    {
+        id: "#svg-mentors",
+        title: "Mentors",
+        placement: "left",
+        content: "FAMNM Mentors are all mentors that work regularly "
+            + "with one or more affiliated team(s) during build season."
+    },
+    {
+        id: "#svg-freelance-mentors",
+        title: "Freelance Mentors",
+        placement: "left",
+        content: "Freelance Mentors are mentors that donate their "
+            + "free time to help teams in need of one-off mentorship. "
+            + "Freelance mentors are not associated with any particular "
+            + "team."
+    },
+    {
+        id: "#svg-volunteers",
+        title: "Volunteers",
+        placement: "left",
+        content: "Volunteers are all FAMNM members who volunteer at "
+            + "FIRST events, and are organized through the Volunteer "
+            + "Coordinator."
+    },
+    {
+        id: "#svg-general-members",
+        title: "General Members",
+        placement: "top",
+        content: "General Members are all U of M students who have "
+            + "joined FAMNM."
+    }
+];
+
+$(window).on("load", () => { 
+    var map = $("#org-pic");
+
+    orgData.forEach(component => {
+        var area = map.find(component.id);
+
+        component.container = $(".content").get(0);
+        component.trigger = "hover click";
+        area.popover(component);
+    });
+});
