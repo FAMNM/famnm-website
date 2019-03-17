61217f2ce645b32363065e7c291e542ea692772f
Eashwar Mohan <eashwar@umich.edu>
2019-03-17 15:13:39 -0400

    begin refactor of JS files

--- js/web-encapsulation.js
+++ js/web-encapsulation.js
@@ -4,89 +4,80 @@
 
 function openInModal (event, title)
 {
-	var href;
-	
-	if (event instanceof Event)
-	{
-		href = event.target.href;
-		event.preventDefault();
-	}
-	else if (event instanceof String)
-		href = event;
-	
-	var href = event.target.href;
-	var modalParts = getLargeModal();
-	var webFrame = document.createElement("iframe");
-	var documentTitle;
+    var href;
+    
+    if (event instanceof Event)
+    {
+        href = event.target.href;
+        event.preventDefault();
+    }
+    else if (event instanceof String)
+        href = event;
+    
+    var href = event.target.href;
+    var modalParts = getLargeModal();
+    var webFrame = document.createElement("iframe");
 
-	webFrame.style.width = "100%";
-	webFrame.style.height = (document.documentElement.clientHeight * 0.65)
-								+ "px";
-	webFrame.src = href;
-	modalParts.body.appendChild(webFrame);
-	
-	document.body.appendChild(modalParts.div);
-	modalParts.div.style.display = "block";
+    webFrame.css("width", "100%");
+    webFrame.css("height", "65vh");
+    webFrame.src = href;
+    modalParts.body.appendChild(webFrame);
+    
+    document.body.appendChild(modalParts.div);
+    modalParts.div.css("display", "block");
 
-	modalParts.footer.appendChild(document.createElement("button"));
-	modalParts.footer.firstElementChild.className = "btn btn-primary";
-	modalParts.footer.firstElementChild.type = "button";
-	modalParts.footer.firstElementChild.setAttribute("data-dismiss", "modal");
-	modalParts.footer.firstElementChild.textContent = "Close";
-	modalParts.footer.firstElementChild.onclick = closeModal;
-	
-	modalParts.div.className += " show";
-	modalParts.header.firstElementChild.textContent = title;
+    appendChildElt($(modalParts.footer), "button")
+    $(modalParts.footer).children.first().addClass("btn btn-primary");
+    $(modalParts.footer).children.first().attr("type", "button");
+    $(modalParts.footer).children.first().attr("data-dismiss", "modal");
+    $(modalParts.footer).children.first().text("Close");
+    $(modalParts.footer).children.first().click(e => $(".modal").first().remove());
+    
+    $(modalParts.div).addClass("show");
+    $(modalParts.header).children.first().text(title);
 }
+
+appendChildElt = (obj, elt) => $(obj).append(document.createElement(elt));
 
 function getLargeModal ()
 {
-	var modalParts = new Object();
-	
-	//Get the divs that compose the modal
-	modalParts.div = document.createElement("div");
-	modalParts.dialog = modalParts.div.appendChild(document.createElement("div"));
-	modalParts.content = modalParts.dialog.appendChild(document.createElement("div"));
-	modalParts.header = modalParts.content.appendChild(document.createElement("div"));
-	modalParts.body = modalParts.content.appendChild(document.createElement("div"));
-	modalParts.footer = modalParts.content.appendChild(document.createElement("div"));
-	
-	//Set modal properties
-	modalParts.div.className = "modal fade bd-example-modal-lg";
-	modalParts.div.id = "large-modal-open";
-	modalParts.div.style.backgroundColor = "rgba(0,0,0,0.5)";
-	modalParts.div.tabIndex = -1;
-	modalParts.div.setAttribute("role", "dialog");
-	modalParts.div.setAttribute("aria-labelledby", "modal-header");
-	modalParts.div.setAttribute("aria-hidden", "true");
-	
-	modalParts.dialog.className = "modal-dialog modal-lg";
-	modalParts.content.className = "modal-content";
-	modalParts.header.className = "modal-header";
-	modalParts.body.className = "modal-body";
-	modalParts.footer.className = "modal-footer";
-	
-	modalParts.header.appendChild(document.createElement("h4"));
-	modalParts.header.firstElementChild.className = "modal-title";
-	modalParts.header.firstElementChild.id = "modal-header";
-	
-	modalParts.header.appendChild(document.createElement("button"));
-	modalParts.header.lastElementChild.type = "button";
-	modalParts.header.lastElementChild.className = "close";
-	modalParts.header.lastElementChild.setAttribute("data-dismiss", "modal");
-	modalParts.header.lastElementChild.setAttribute("aria-label", "Close");
-	modalParts.header.lastElementChild.appendChild(document.createElement("span"));
-	modalParts.header.lastElementChild.lastElementChild.textContent = "\u2a09";
-	modalParts.header.lastElementChild.onclick = closeModal;
-	
-	return modalParts;
-}
-
-function closeModal (event)
-{
-	event = event || window.event;
-	
-	var modalWindow = document.getElementsByClassName("modal")[0];
-	
-	modalWindow.parentElement.removeChild(modalWindow);
+    var modalParts = new Object();
+    
+    //Get the divs that compose the modal
+    modalParts.div = document.createElement("div");
+    modalParts.dialog = appendChildElt($(modalParts.div), "div")
+    modalParts.content = appendChildElt($(modalParts.dialog), "div")
+    modalParts.header = appendChildElt($(modalParts.content), "div")
+    modalParts.body = appendChildElt($(modalParts.content), "div")
+    modalParts.footer = appendChildElt($(modalParts.content), "div")
+    
+    //Set modal properties
+    $(modalParts.div).addClass("modal fade bd-example-modal-lg");
+    $(modalParts.div).attr("id", "large-modal-open");
+    $(modalParts.div).css("backgroundColor", "rgba(0,0,0,0.5)");
+    $(modalParts.div).attr("tabIndex", -1);
+    $(modalParts.div).attr("role", "dialog");
+    $(modalParts.div).attr("aria-labelledby", "modal-header");
+    $(modalParts.div).attr("aria-hidden", "true");
+    
+    $(modalParts.dialog).addClass("modal-dialog modal-lg");
+    $(modalParts.content).addClass("modal-content");
+    $(modalParts.header).addClass("modal-header");
+    $(modalParts.body).addClass("modal-body");
+    $(modalParts.footer).addClass("modal-footer");
+    
+    appendChildElt($(modalParts.header), "h4")
+    $(modalParts.header).children.first().addClass("modal-title");
+    $(modalParts.header).children.first().addClass("id", "modal-header");
+    
+    appendChildElt($(modalParts.header), "button")
+    $(modalParts.header).children.last().attr("type", "button");
+    $(modalParts.header).children.last().addClass("close");
+    $(modalParts.header).children.last().attr("data-dismiss", "modal");
+    $(modalParts.header).children.last().attr("aria-label", "Close");
+    appendChildElt($(modalParts.header).children.last(), "span");
+    $(modalParts.header).children.last().children.last().text("\u2a09");
+    $(modalParts.header).children.last().click(e => $(".modal").first().remove());
+    
+    return modalParts;
 }
