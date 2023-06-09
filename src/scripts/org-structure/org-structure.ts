d18f725c2e02a28703ead5dc582aa58a0bab2f6e
yottalogical <email@yottalogical.com>
2023-06-08 22:40:43 -0400

    Put org-structure orgData in a separate JSON file

--- src/scripts/org-structure/org-structure.ts
+++ src/scripts/org-structure/org-structure.ts
@@ -1,28 +1,17 @@
+import orgData from './org-data.json';
+
 const isMobile = /Mobi|Android/i.test(navigator.userAgent);
 
-const orgDataString = document.getElementById('org-structure-script')?.dataset.orgdata;
+orgData.forEach(entry => {
+    const svgElement = document.getElementById(entry.id);
 
-if (orgDataString !== undefined) {
-    const orgData: [{
-        id: string,
-        title: string,
-        placement: string,
-        content: string,
-    }] = JSON.parse(orgDataString);
-
-    orgData.forEach(entry => {
-        const svgElement = document.getElementById(entry.id);
-
-        if (svgElement !== null) {
-            svgElement.setAttribute('data-bs-toggle', 'popover');
-            svgElement.setAttribute('data-bs-title', entry.title);
-            svgElement.setAttribute('data-bs-placement', isMobile ? 'top' : entry.placement);
-            svgElement.setAttribute('data-bs-content', entry.content);
-            svgElement.setAttribute('data-bs-trigger', isMobile ? 'click' : 'hover');
-        } else {
-            console.log(`Error: Element with id ${entry.id} does not exist`);
-        }
-    });
-} else {
-    console.log('Error: orgData not present');
-}
+    if (svgElement !== null) {
+        svgElement.setAttribute('data-bs-toggle', 'popover');
+        svgElement.setAttribute('data-bs-title', entry.title);
+        svgElement.setAttribute('data-bs-placement', isMobile ? 'top' : entry.placement);
+        svgElement.setAttribute('data-bs-content', entry.content);
+        svgElement.setAttribute('data-bs-trigger', isMobile ? 'click' : 'hover');
+    } else {
+        console.log(`Error: Element with id ${entry.id} does not exist`);
+    }
+});
