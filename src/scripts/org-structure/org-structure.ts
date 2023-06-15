bc5415106f1d13b096e90769da67b84a2b330f7f
yottalogical <email@yottalogical.com>
2023-06-15 08:55:35 -0400

    Apply ESLint to org-structure.ts

--- src/scripts/org-structure/org-structure.ts
+++ src/scripts/org-structure/org-structure.ts
@@ -1,21 +1,21 @@
+import * as bootstrap from 'bootstrap';
 import orgData from './org-data.json';
-import * as bootstrap from "bootstrap";
 
 const isMobile = /Mobi|Android/i.test(navigator.userAgent);
 
-orgData.forEach(entry => {
-    const svgElement = document.getElementById(entry.id);
+orgData.forEach((entry) => {
+  const svgElement = document.getElementById(entry.id);
 
-    if (svgElement !== null) {
-        svgElement.setAttribute('data-bs-toggle', 'popover');
-        svgElement.setAttribute('data-bs-title', entry.title);
-        svgElement.setAttribute('data-bs-placement', isMobile ? 'top' : entry.placement);
-        svgElement.setAttribute('data-bs-content', entry.content);
-        svgElement.setAttribute('data-bs-trigger', isMobile ? 'click' : 'hover');
-    } else {
-        console.log(`Error: Element with id ${entry.id} does not exist`);
-    }
+  if (svgElement !== null) {
+    svgElement.setAttribute('data-bs-toggle', 'popover');
+    svgElement.setAttribute('data-bs-title', entry.title);
+    svgElement.setAttribute('data-bs-placement', isMobile ? 'top' : entry.placement);
+    svgElement.setAttribute('data-bs-content', entry.content);
+    svgElement.setAttribute('data-bs-trigger', isMobile ? 'click' : 'hover');
+  } else {
+    throw Error(`Error: Element with id ${entry.id} does not exist`);
+  }
 });
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-[...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+[...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
