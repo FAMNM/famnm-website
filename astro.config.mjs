42d51a3fde369061772654930b6220d3315e146a
Michael Peng <michael@mcmoo.org>
2024-05-08 14:32:49 -0400

    Remove Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, react()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  integrations: [astroImageTools, react()]
 });
