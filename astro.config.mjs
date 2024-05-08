944296d2efc188386f20df9471c5130badad8ecb
Michael Peng <michael@mcmoo.org>
2024-05-08 12:27:52 -0400

    Update packages and add Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, react()]
+  integrations: [astroImageTools, react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
